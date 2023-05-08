78d5fd47a0ade2a3797120c86e577cda8956c67d
ronnimadeira <115476262+ronnimadeira@users.noreply.github.com>
2023-05-08 14:34:18 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -2,5 +2,13 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     radio.sendString("A")
 })
-basic.showString("A")
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    radio.sendString("B")
+})
 radio.setGroup(5)
+basic.showIcon(IconNames.Chessboard)
